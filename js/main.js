01fdef8d237ab96b1391c01b7a847801a837d025
velemon <143354515+velemon@users.noreply.github.com>
2026-06-20 20:56:37 +0200

    funktion för att skapa HTML element för varje rätt

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// main.js - Denna fil innehåller logiken för att hantera användarinteraktioner och uppdatera gränssnittet
+
+// Importera funktioner från api.js
+async function loadMenu() {
+
+    // Hämta menyn från backend-API:et
+    const menu = await getMenu();
+    const container = document.getElementById("menu");
+
+    // Loopa genom menyn och skapa HTML-element för varje maträtt
+    menu.forEach(item => {
+        container.innerHTML += `
+            <div class="card">
+                <h3>${item.name}</h3>
+                <p>${item.description}</p>
+                <p><b>${item.marinade}</b></p>
+                <p>${item.price} kr</p>
+            </div>
+        `;
+    });
+}
+
+// Ladda menyn när sidan laddas
+loadMenu();
